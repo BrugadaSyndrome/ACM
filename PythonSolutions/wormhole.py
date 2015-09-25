d538e1fcb031c6c20bcbb68e1f85bec3e95bd0a5
Coby Johnson <i2c2.caj@gmail.com>
2015-09-25 16:56:52 -0600

    wormholes: some simplifications
    There are still issues with the walk_wormholes method. Line 52 needs a
    return state..., and line 41 return result never gets evaluated...

--- PythonSolutions/wormhole.py
+++ PythonSolutions/wormhole.py
@@ -10,12 +10,6 @@ def parse_planet(planet_string):
     z = int(z)
     return name, (x, y, z)
 
-def parse_wormhole(wormhole_string):
-    return wormhole_string.split()
-
-def parse_query(query_string):
-    return query_string.split()
-
 def distance(p1, p2, planets):
     p1x, p1y, p1z = planets[p1]
     p2x, p2y, p2z = planets[p2]
@@ -24,19 +18,13 @@ def distance(p1, p2, planets):
 def closest_distance(p1, p2, planets, wormholes):
     # is there a free jump
     wh = wormholes.copy()
-    result, dist = free_jump(p1, p2, [], wh, planets)
-    if (result == True):
-        return 0
-    else:
-        return dist
-
-    #return distance(p1, p2, planets)
+    return walk_wormholes(p1, p2, [], wh, planets)
 
 #'walk' the wormholes
-def free_jump(start, goal, visited, wormholes, planets):
+def walk_wormholes(start, goal, visited, wormholes, planets):
     if (start == goal):
         #print 'Found it!'
-        return (True, 0)
+        return 0
 
     #ln 1 will most likely get key errors
     #print visited, "=>", start, "?", wormholes[start], '? =>', goal, '---', wormholes
@@ -50,22 +38,22 @@ def free_jump(start, goal, visited, wormholes, planets):
             # make sure to not loop to previously visited planets
             if (P not in visited):
                 #print start, '=>',
-                result, best_dist = free_jump(P, goal, visited+[start], wormholes, planets)
+                best_dist = walk_wormholes(P, goal, visited+[start], wormholes, planets)
                 next_dist = distance(P, goal, planets)
                 #print '{0} < {1} ? {2}'.format(cur_dist, best_dist, cur_dist<best_dist)
                 if (cur_dist < next_dist):
-                    return (result, cur_dist)
+                    return cur_dist
                 else:
-                    return (result, next_dist)
+                    return next_dist
             else:
                 #print "BEEN TO NEXT BEFORE BEFORE! next:", P,
                 L = wormholes[start]
-                #print 'CUR:', start ,'WHL:', L[1:]
                 wormholes[start] = L[1:]
-                result, best_dist = free_jump(start, goal, visited, wormholes, planets)
+                best_dist = walk_wormholes(start, goal, visited, wormholes, planets)
+                return best_dist
     
     #print 'Dead End'
-    return (False, cur_dist)
+    return cur_dist
 
 def main():
     num_cases = int(sys.stdin.readline().strip())
@@ -82,7 +70,7 @@ def main():
         num_wormholes = int(sys.stdin.readline().strip())
         wormholes = {}
         for wormhole in range(num_wormholes):
-            from_p, to_p = parse_wormhole(sys.stdin.readline().strip())
+            from_p, to_p = sys.stdin.readline().strip().split()
             if (from_p in wormholes.keys()):
                 L = wormholes[from_p]
                 wormholes[from_p] = L + [to_p]
@@ -95,7 +83,7 @@ def main():
         num_queries = int(sys.stdin.readline().strip())
         queries = []
         for query in range(num_queries):
-            queries.append(parse_query(sys.stdin.readline().strip()))
+            queries.append(sys.stdin.readline().strip().split())
 
         print "Case {0}:".format(case+1)
         for q in queries:
