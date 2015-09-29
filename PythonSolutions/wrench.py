ba4a3ec79e8b5ec1361d8175f70acee95ceb0303
Coby Johnson <i2c2.caj@gmail.com>
2015-09-28 19:24:39 -0600

    solved wrenches

--- PythonSolutions/wrench.py
+++ PythonSolutions/wrench.py
@@ -2,35 +2,41 @@ import sys
 import re
 from decimal import Decimal
 from fractions import Fraction
+import math
 
-def find_fraction(dec):
+def equal(D1, D2, ACC):
+    RU = math.ceil(D2*pow(10, ACC))
+    RD = math.floor(D2*pow(10, ACC))
+    #print D2, RU, RD
+    if (D1 in [RU, RD]):
+        return True
+    return False
+
+def dec_eq_fract(dec, fract):
+    L = len(dec)
+    int_dec = int(dec)
+    for f in fract:
+        if (equal(int_dec, f[1], L)):
+            return f[0]
+    return -1
+
+def generate_fractions():
+    fracts = []
     num = 1
     den = 1
-    best_num = 129
-    best_den = 129
-    best_diff = 11
     for i in range(7):
         den *= 2
         for j in range(den-1):
-            compare = '{0}./{1}'.format(num+j, den)
-            ev = eval(compare) - dec
-            print compare, eval(compare), dec, ev
-            # difference is small enough and positive
-            if (ev >= 0 and ev < .009):
-            #if (ev >= 0):
-            #if (abs(ev) < .009):
-            
-                if (best_diff > ev):
-                    print '***', compare, eval(compare), dec, eval(compare) - dec
-                    best_num = num+j
-                    best_den = den
-                    best_diff = ev
+            fract = '{0}/{1}'.format(num+j, den)
+            dec = eval('{0}./{1}'.format(num+j, den))
+            fracts.append((fract, dec))
         
-    return '{0}/{1}'.format(best_num, best_den)
+    return fracts
         
 
 def main():
     cases = int(sys.stdin.readline().strip())
+    fracts = generate_fractions()
 
     for c in range(cases):
         decimal = sys.stdin.readline().strip()
@@ -41,17 +47,14 @@ def main():
             # real number
             if (match.group(3) == '' or int(match.group(3)) == 0 ):
                 print '{0}"'.format(int(match.group(1)))
-                print
             # decimal number
             elif (match.group(1) == '' or int(match.group(1)) == 0):
                 #print '? {0}"'.format(match.group(3))
-                print '{0}"'.format(find_fraction(float('.{0}'.format(match.group(3)))))
-                print
+                print '{0}"'.format(dec_eq_fract(match.group(3), fracts))
             # improper number
             else:
                 #print '? {0} {1}"'.format(match.group(1), match.group(3))
-                print '? {0} {1}"'.format(match.group(1), find_fraction(float('.{0}'.format(match.group(3)))))
-                print
+                print '{0} {1}"'.format(match.group(1), dec_eq_fract(match.group(3), fracts))
         else:
             print 'Input does not match: {0}'.format(decimal)
             
