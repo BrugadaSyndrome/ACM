cc82d2188ae8b599d6379cc6f7c6adc2b3545fcf
Coby Johnson <i2c2.caj@gmail.com>
2015-10-09 16:17:13 -0600

    town squares
    working on it a bit

--- PythonSolutions/unsolved/town_square.py
+++ PythonSolutions/unsolved/town_square.py
@@ -1,8 +1,26 @@
+import math
 import sys
+
+def slope(p1, p2):
+    num = p2[1]-p1[1]
+    den = p2[0]-p1[0]
+    if (den == 0):
+        return 'U'
+    #print p1, p2, num, den
+    return num/den
+
+def point_distance(p1, p2):
+    a = p2[1]-p1[1]
+    b = p2[0]-p1[0]
+    c = math.sqrt(pow(a, 2) + pow(b, 2))
+    return c
+
+def sin_distance(d, theta, phi):
+    return (math.sin(theta) * math.sin(phi)) / d
 
 def fence_it(positions):
 
-    # find furthest points that must be fenced
+    # determine the furthest points
     left = (101, 0)
     right = (-101, 0)
     top = (0, -101)
@@ -17,23 +35,44 @@ def fence_it(positions):
         if (p[1] < bottom[1]):
             bottom = p
 
-    # get side length
-    s1 = abs(right[0] - left[0])
-    s2 = abs(top[1] - bottom[1])
+    #print 'L:{0} R:{1} B:{2} T:{3}'.format(left, right, bottom, top)
 
-    #print left, right
-    #print bottom, top
-    #print s1, s2
+    # adjust so that origin is equal to bottom
+    adjust = bottom
+    left = (left[0]-adjust[0], left[1]-adjust[1])
+    right = (right[0]-adjust[0], right[1]-adjust[1])
+    bottom = (bottom[0]-adjust[0], bottom[1]-adjust[1])
+    top = (top[0]-adjust[0], top[1]-adjust[1])
+    min_slope = slope(left, bottom)
+    max_slope = slope(bottom, right)
 
-    # invalid
-    if (s1 == 0 or s2 == 0):
-        return 'no solution'
+    # min/max slopes
+    if (min_slope == 'U'):
+        min_slope = -200
+    if (max_slope == 'U'):
+        max_slope = 200
 
-    #valid, return longest side + 10 ft padding
-    if (s1 > s2):
-        return '{0:.2f}'.format(s1+10)
-    else:
-        return '{0:.2f}'.format(s2+10)
+    print 'La:{0} Ra:{1} Ba:{2} Ta:{3}'.format(left, right, bottom, top)
+    print 'min-slope:{0} right-slope:{1}'.format(min_slope, max_slope)
+
+    slopes = []
+    begin = min_slope
+    end = max_slope
+    step = 0.1
+    while (begin <= end):
+        slopes.append(int(begin*10)/10.0)
+        begin = begin + step
+
+    '''  
+    for s in slopes:
+        d1 = point_distance(bottom, left)
+        d2 = sin_distance(d1, 90, s)
+        print 'd1:{0} d2:{1} d3:{2}'.format(d1, d2, d3)
+    '''
+        
+    print slopes
+    
+    return 'no solution'
 
 def main():
     cases = int(sys.stdin.readline().strip())
@@ -43,7 +82,8 @@ def main():
         x1, y1, x2, y2, x3, y3, x4, y4 = positions.split()
 
         print 'Case {0}: {1}'.format(c+1, fence_it([(int(x1), int(y1)), (int(x2), int(y2)), (int(x3), int(y3)), (int(x4), int(y4))]))
-
+        print
+        
 main()
 
 #no solution
