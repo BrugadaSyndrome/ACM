161109a86c5f3ef97bf0aa2235557544281428f9
Coby Johnson <i2c2.caj@gmail.com>
2016-05-16 18:53:41 -0600

    Subfolders created when PDF files are added

--- Grader/Grader/Program.cs
+++ Grader/Grader/Program.cs
@@ -29,87 +29,77 @@ namespace Grader
                     problem2-2.in
                     problem2-2.out
             /Solutions
-                /Language1
-                    problem1.???
-                /Language2
-                    problem2.???
+                /problem1
+                    problem1.py
+                    problem1.etc
+                /problem2
+                    problem2.py
+                    problem2.etc
             Grader.exe
             /Problems
                 problem1.pdf
                 problem2.pdf
         */
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             // Setup necessary folders
             Setup();
             
             // Grade problems
-            System.IO.DirectoryInfo problems = new System.IO.DirectoryInfo(Path.Combine(acm_folder, problem_folder));
-            foreach (DirectoryInfo prob in problems.EnumerateDirectories())
-            {
-                Console.WriteLine("Problem Directories: {0}", prob.Name);
+            System.IO.DirectoryInfo answers = new System.IO.DirectoryInfo(Path.Combine(acm_folder, answer_folder));
+            foreach (DirectoryInfo ans in answers.EnumerateDirectories()) {
+                Console.WriteLine("Problem: {0}", ans.Name);
+
+                System.IO.DirectoryInfo solutions = new System.IO.DirectoryInfo(Path.Combine(acm_folder, solution_folder, ans.Name));
+                foreach (FileInfo file in solutions.EnumerateFiles()) {
+                    Console.WriteLine("       + {0}", file.Name);
+                }
             }
-
-            /*
-            System.IO.DirectoryInfo solutions = new System.IO.DirectoryInfo(python_solution_directory);
-            foreach (FileInfo file in solutions.EnumerateFiles())
-            {
-                Console.WriteLine("Python File: {0}", file.Name);
-            }
-            */
-
-
         }
 
-        private static bool CreateFolder(string folder)
-        {
-            if (!Directory.Exists(folder))
-            {
+        private static bool CreateFolder(string folder) {
+            if (!Directory.Exists(folder)) {
                 Directory.CreateDirectory(folder);
                 if (DEBUG)
                     Console.WriteLine("Folder \"{0}\" created.", folder);
                 return true;
             }
-            else
-            {
+            else {
                 if (DEBUG)
                     Console.WriteLine("Folder \"{0}\" already exists.", folder);
                 return false;
             }
         }
 
-        private static void Setup()
-        {
+        private static void Setup() {
             if (DEBUG)
                 Console.WriteLine("Setup started.");
 
-            int files_created = 0;
+            int folders_created = 0;
 
             // Create necessary folders
-            if (CreateFolder(Path.Combine(acm_folder, answer_folder))) files_created++;
-            if (CreateFolder(Path.Combine(acm_folder, problem_folder))) files_created++;
-            if (CreateFolder(Path.Combine(acm_folder, solution_folder))) files_created++;
+            if (CreateFolder(Path.Combine(acm_folder, answer_folder)))
+                folders_created++;
+            if (CreateFolder(Path.Combine(acm_folder, problem_folder)))
+                folders_created++;
+            if (CreateFolder(Path.Combine(acm_folder, solution_folder)))
+                folders_created++;
 
-            // Create a sub-directory for each item in languages[]
-            foreach (string folder in languages)
-            {
-                if (CreateFolder(Path.Combine(acm_folder, "Solutions", folder))) files_created++;
-            }
-
-            // Create a sub-directory in answers for each problem.pdf file in the Problems folder
-            System.IO.DirectoryInfo problem_directory = new System.IO.DirectoryInfo(Path.Combine(acm_folder, "Problems"));
-            foreach (FileInfo file in problem_directory.EnumerateFiles())
-            {
-                if (file.Extension == ".pdf")
-                {
-                    if (CreateFolder(Path.Combine(acm_folder, answer_folder, Path.GetFileNameWithoutExtension(file.Name)))) files_created++;
+            // Create subdirectories in the Answers and Solutions folders for each pdf file in the Problems folder
+            System.IO.DirectoryInfo problem_directory = new System.IO.DirectoryInfo(Path.Combine(acm_folder, problem_folder));
+            foreach (FileInfo file in problem_directory.EnumerateFiles()) {
+                if (file.Extension == ".pdf") {
+                    if (CreateFolder(Path.Combine(acm_folder, answer_folder, Path.GetFileNameWithoutExtension(file.Name))))
+                        folders_created++;
+                    if (CreateFolder(Path.Combine(acm_folder, solution_folder, Path.GetFileNameWithoutExtension(file.Name))))
+                        folders_created++;
                 }
                 else if (DEBUG)
                     Console.WriteLine("File found in Problems Directory that is not a PDF: {0}", file.Name);
             }
 
-            Console.WriteLine("Setup Done. Created {0} folders.", files_created);
+            if (DEBUG)
+                Console.WriteLine("Created {0} folders. \nSetup Done", folders_created);
         }
 
     }
